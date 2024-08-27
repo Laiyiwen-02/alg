09b2ad6b62bc12e95f187643c17eff46fe6f3e94
Laiyiwen-02 <146494090+Laiyiwen-02@users.noreply.github.com>
2024-08-27 01:05:41 +0000

    add alg

--- editor.php
+++ editor.php
@@ -31,15 +31,6 @@ body {
 // Proxy Monaco Editor workers using a data URL (adapted from: https://github.com/microsoft/monaco-editor/blob/main/docs/integrate-amd-cross.md)
 require.config({ paths: { "vs": "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/vs/" }});
 
-window.MonacoEnvironment = {
-    getWorkerUrl: function(workerId, label) {
-        return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
-            self.MonacoEnvironment = { baseUrl: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/" };
-            importScripts("https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/vs/base/worker/workerMain.min.js");`
-        )}`;
-    }
-};
-
 require(["vs/editor/editor.main"], function () {
     // Create the editor with some sample JavaScript code
     var editor = monaco.editor.create(document.getElementById("editor"), {
