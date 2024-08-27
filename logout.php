172599fa88d7a57837793fa061d232863250ea4c
Laiyiwen-02 <146494090+Laiyiwen-02@users.noreply.github.com>
2024-08-27 00:45:04 +0000

    add alg

--- logout.php
+++ logout.php
@@ -4,7 +4,8 @@ if(isset($_SESSION['user'])&&isset($_SESSION['pwd'])){
 unset($_SESSION['user']);
 unset($_SESSION['pwd']);
 echo "<script>alert('登出成功');window.location.href='./';</script>";
-}else{
+}
+else{
   echo "<script>alert('出错了！你还未登录！');window.location.href='/login.php';</script>";
 }
 ?>
