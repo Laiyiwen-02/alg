5569cc7163e368400bf96299e75e2778ad66ce6c
Laiyiwen-02 <146494090+Laiyiwen-02@users.noreply.github.com>
2025-05-19 06:04:52 +0000

    add alg

--- discuss/post/1.php
+++ discuss/post/1.php
@@ -1,18 +1,4 @@
 <?php session_start();?><!DOCTYPE html><html><head><title>hello world</title><?php include "../../menu.php"; ?><link rel="stylesheet" href="https://use.sevencdn.com/npm/katex@0.16.11/dist/katex.min.css"></head><body><div class = "layui-panel" style = "padding:2%; margin:2%; color:black;"><h1>hello world</h1></div><div class = "layui-panel markdown-body" style = "padding:2%; margin:2%;"><h1>Hello World</h1>
 <p>Welcome CLOJ discuss!!!</p>
 <p>$\texttt{zx \&amp; ctj 99!!!!!} \\ O(n \log n)$</p>
-</div><script src="https://use.sevencdn.com/npm/katex@0.16.11/dist/katex.min.js"></script><script src="https://use.sevencdn.com/npm/katex@0.16.11/dist/contrib/auto-render.min.js" onload = 'renderMathInElement(document.body, {delimiters: [{left: "$$", right: "$$", display: true}, {left: "$", right: "$", display: false} ], macros: {"\\geq": "\\geqslant", "\\leq": "\\leqslant" } }); '></script><script src="https://giscus.app/client.js"
-        data-repo="Laiyiwen-02/giscus"
-        data-repo-id="R_kgDOMhxnTw"
-        data-category="Announcements"
-        data-category-id="DIC_kwDOMhxnT84ChhxH"
-        data-mapping="url"
-        data-strict="0"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="bottom"
-        data-theme="preferred_color_scheme"
-        data-lang="zh-CN"
-        crossorigin="anonymous"
-        async>
-</script></body></html>
+</div><script src="https://use.sevencdn.com/npm/katex@0.16.11/dist/katex.min.js"></script><script src="https://use.sevencdn.com/npm/katex@0.16.11/dist/contrib/auto-render.min.js" onload = 'renderMathInElement(document.body, {delimiters: [{left: "$$", right: "$$", display: true}, {left: "$", right: "$", display: false} ], macros: {"\\geq": "\\geqslant", "\\leq": "\\leqslant" } }); '></script><div class = "layui-panel" style = "padding: 2%; margin: 2%;"><script src="https://giscus.app/client.js" data-repo="Laiyiwen-02/giscus" data-repo-id="R_kgDOMhxnTw" data-category="Announcements" data-category-id="DIC_kwDOMhxnT84ChhxH" data-mapping="url" data-strict="0" data-reactions-enabled="1" data-emit-metadata="0" data-input-position="bottom" data-theme="preferred_color_scheme" data-lang="zh-CN" crossorigin="anonymous" async></script></div></body></html>
